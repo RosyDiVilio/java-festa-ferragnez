f2586119002d4aa2b0f98e2237a7700bfbea9f1d
RosyDiVilio <165782561+RosyDiVilio@users.noreply.github.com>
2024-06-12 11:19:48 +0200

    add: lista party completata

--- src/main/java/com/ferragnez/party/CheckGuest.java
+++ src/main/java/com/ferragnez/party/CheckGuest.java
@@ -10,20 +10,27 @@ public class CheckGuest {
 		System.out.println("Come ti chiami?");
 		String userName = inputUser.nextLine();
 		
-		inputUser.close();
+		
 		
 		String[] invitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
 		
+		boolean invitatiFesta = false;
+		
 		for (int index = 0; index < invitati.length; index++) {
-			
-		   if (userName == invitati[index]) {
-			  System.out.println("Complimenti sei stato invitato al party!");  
-           } else {
+			 if (userName.equals(invitati[index])) {
+			   invitatiFesta = true;
+			   break;
+			 }
+		}
+			 
+			 if (invitatiFesta == true) {
+			  System.out.println("Complimenti sei stato invitato al party!"); 
+             } else {
         	  System.out.println("Mi dispiace non sei stato invitato al party");
-           }
-	
+             }
+			 
+			 inputUser.close();
 	}
 
   }
 	
-}
